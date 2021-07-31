7b819a02a9519f4ce467bb3a2ea3efba64e2c9f7
bonthusaireddy <bonthusaireddy@gmail.com>
2021-07-31 17:34:22 +0530

    Update capture.php

--- capture.php
+++ capture.php
@@ -361,7 +361,7 @@ else
 	}
 
 	$sql = "INSERT INTO surveys(project_id,vendor_id,status,ip_address,vendor_resp_id,client_resp_id) VALUES ('$gid','$vid','start','$ip','$pid','$pid')";
-        
+    if(isset($_POST['gid'] && isset($_POST['vid']) ) ){
     if ($conn->query($sql) === TRUE) {
     echo "data entered";
     $sql2 = "SELECT survey_link FROM surveys INNER JOIN projects ON surveys.project_id = projects.id WHERE surveys.project_id = '$gid'";
@@ -369,7 +369,7 @@ else
 
 
     echo 'asdf';
-} else {
+}} else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
