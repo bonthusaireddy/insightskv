4abdd1aeb7ab0b78b64e55ba1954bfc2579c7fbc
bonthusaireddy <52176773+bonthusaireddy@users.noreply.github.com>
2021-08-10 02:05:39 +0530

    Update capture.php
    Proper capturing completed.

--- capture.php
+++ capture.php
@@ -1,397 +1,98 @@
-<!DOCTYPE html>
 <html>
-<head>
-	<title>start</title>
-    <link rel="shortcut icon" href="flogo.png">
-    
-    <link href='https://fonts.googleapis.com/css?family=Raleway:200,400,800' rel='stylesheet' type='text/css'>
-    
-   <link href="https://www.marcoguglie.it/Codepen/AnimatedHeaderBg/demo-1/css/demo.css"  type='text/css'>
-   
-   <script type="text/javascript" src="https://www.marcoguglie.it/Codepen/AnimatedHeaderBg/demo-1/js/EasePack.min.js"></script>
-   <script type="text/javascript" src="https://www.marcoguglie.it/Codepen/AnimatedHeaderBg/demo-1/js/TweenLite.min.js"></script>
-   <script type="text/javascript" src="https://www.marcoguglie.it/Codepen/AnimatedHeaderBg/demo-1/js/rAF.js"></script>
-   
-     
-   
-</head>
-
-<div id="large-header" class="large-header">
-<!-- HEADER -->
-        <header>
-            
-            <!-- MENU BLOCK -->
-            <div class="menu_block">
-            
-                <!-- CONTAINER -->
-                <div class="container clearfix">
-                    
-                    <!-- LOGO -->
-                    <div class="logo pull-left">
-                        <a href="index.php" ><img src="finallogo.png" alt="logo"></a>
-                    </div><!-- //LOGO -->
-                    
-                    <!-- SEARCH FORM -->
-                    <!-- SEARCH FORM -->
-                    
-                    
-            </div><!-- //CONTAINER -->
-        </header><!-- //HEADER -->
-
-  <canvas id="demo-canvas"></canvas>
-    <h1 class="main-title"><font size="12">COMPLETE !</font><br><span class="thin"><h6> Thank You for taking the time to complete this survey. We truly value the information you have provided. Your responses will contribute to our analyses of the texts and suggest new lines of approach to the corpus data.</h6></span></h1>
-</div>
-
-
-
-
-
-<script>
-
-(function() {
-
-    var width, height, largeHeader, canvas, ctx, points, target, animateHeader = true;
-
-    // Main
-    initHeader();
-    initAnimation();
-    addListeners();
-
-    function initHeader() {
-        width = window.innerWidth;
-        height = window.innerHeight;
-        target = {x: width/2, y: height/2};
-
-        largeHeader = document.getElementById('large-header');
-        largeHeader.style.height = height+'px';
-
-        canvas = document.getElementById('demo-canvas');
-        canvas.width = width;
-        canvas.height = height;
-        ctx = canvas.getContext('2d');
-
-        // create points
-        points = [];
-        for(var x = 0; x < width; x = x + width/20) {
-            for(var y = 0; y < height; y = y + height/20) {
-                var px = x + Math.random()*width/20;
-                var py = y + Math.random()*height/20;
-                var p = {x: px, originX: px, y: py, originY: py };
-                points.push(p);
-            }
-        }
-
-        // for each point find the 5 closest points
-        for(var i = 0; i < points.length; i++) {
-            var closest = [];
-            var p1 = points[i];
-            for(var j = 0; j < points.length; j++) {
-                var p2 = points[j]
-                if(!(p1 == p2)) {
-                    var placed = false;
-                    for(var k = 0; k < 5; k++) {
-                        if(!placed) {
-                            if(closest[k] == undefined) {
-                                closest[k] = p2;
-                                placed = true;
-                            }
-                        }
-                    }
-
-                    for(var k = 0; k < 5; k++) {
-                        if(!placed) {
-                            if(getDistance(p1, p2) < getDistance(p1, closest[k])) {
-                                closest[k] = p2;
-                                placed = true;
-                            }
-                        }
-                    }
-                }
-            }
-            p1.closest = closest;
-        }
-
-        // assign a circle to each point
-        for(var i in points) {
-            var c = new Circle(points[i], 2+Math.random()*2, 'rgba(255,255,255,0.3)');
-            points[i].circle = c;
-        }
-    }
-
-    // Event handling
-    function addListeners() {
-        if(!('ontouchstart' in window)) {
-            window.addEventListener('mousemove', mouseMove);
-        }
-        window.addEventListener('scroll', scrollCheck);
-        window.addEventListener('resize', resize);
-    }
-
-    function mouseMove(e) {
-        var posx = posy = 0;
-        if (e.pageX || e.pageY) {
-            posx = e.pageX;
-            posy = e.pageY;
-        }
-        else if (e.clientX || e.clientY)    {
-            posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-            posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-        }
-        target.x = posx;
-        target.y = posy;
-    }
-
-    function scrollCheck() {
-        if(document.body.scrollTop > height) animateHeader = false;
-        else animateHeader = true;
-    }
-
-    function resize() {
-        width = window.innerWidth;
-        height = window.innerHeight;
-        largeHeader.style.height = height+'px';
-        canvas.width = width;
-        canvas.height = height;
-    }
-
-    // animation
-    function initAnimation() {
-        animate();
-        for(var i in points) {
-            shiftPoint(points[i]);
-        }
-    }
-
-    function animate() {
-        if(animateHeader) {
-            ctx.clearRect(0,0,width,height);
-            for(var i in points) {
-                // detect points in range
-                if(Math.abs(getDistance(target, points[i])) < 4000) {
-                    points[i].active = 0.3;
-                    points[i].circle.active = 0.6;
-                } else if(Math.abs(getDistance(target, points[i])) < 20000) {
-                    points[i].active = 0.1;
-                    points[i].circle.active = 0.3;
-                } else if(Math.abs(getDistance(target, points[i])) < 40000) {
-                    points[i].active = 0.02;
-                    points[i].circle.active = 0.1;
-                } else {
-                    points[i].active = 0;
-                    points[i].circle.active = 0;
-                }
-
-                drawLines(points[i]);
-                points[i].circle.draw();
-            }
-        }
-        requestAnimationFrame(animate);
-    }
-
-    function shiftPoint(p) {
-        TweenLite.to(p, 1+1*Math.random(), {x:p.originX-50+Math.random()*100,
-            y: p.originY-50+Math.random()*100, ease:Circ.easeInOut,
-            onComplete: function() {
-                shiftPoint(p);
-            }});
-    }
-
-    // Canvas manipulation
-    function drawLines(p) {
-        if(!p.active) return;
-        for(var i in p.closest) {
-            ctx.beginPath();
-            ctx.moveTo(p.x, p.y);
-            ctx.lineTo(p.closest[i].x, p.closest[i].y);
-            ctx.strokeStyle = 'rgba(156,217,249,'+ p.active+')';
-            ctx.stroke();
-        }
-    }
-
-    function Circle(pos,rad,color) {
-        var _this = this;
-
-        // constructor
-        (function() {
-            _this.pos = pos || null;
-            _this.radius = rad || null;
-            _this.color = color || null;
-        })();
-
-        this.draw = function() {
-            if(!_this.active) return;
-            ctx.beginPath();
-            ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
-            ctx.fillStyle = 'rgba(156,217,249,'+ _this.active+')';
-            ctx.fill();
-        };
-    }
-
-    // Util
-    function getDistance(p1, p2) {
-        return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
-    }
-    
-})();
-
-
-</script>
-
-
-
-
-
-<style>
-/* Header */
-.large-header {
-	position: relative;
-	width: 100%;
-	background: #333;
-	overflow: hidden;
-	background-size: cover;
-	background-position: center center;
-	z-index: 1;
-}
-
-#large-header {
-	background-image: url('https://www.marcoguglie.it/Codepen/AnimatedHeaderBg/demo-1/img/demo-1-bg.jpg');
-}
-
-.main-title {
-	position: absolute;
-	margin: 0;
-	padding: 0;
-	color: #f9f1e9;
-	text-align: center;
-	top: 50%;
-	left: 50%;
-	-webkit-transform: translate3d(-50%,-50%,0);
-	transform: translate3d(-50%,-50%,0);
-}
-
-.demo-1 .main-title {
-	text-transform: uppercase;
-	font-size: 6.2em;
-	letter-spacing: 0.1em;
-}
-
-.main-title .thin {
-	font-weight: 200;
-}
-
-@media only screen and (max-width : 768px) {
-	.demo-1 .main-title {
-		font-size: 3em;
-	}
-}
-
-
-
-</style>
-<body>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<?php
- 	include('includes/imp/conn.php');
-	
-
-    $pid = $_GET['pid'];
-    $client_resp_id = $pid;
-    $gid = $_GET['gid']; //project id
+ <body>
+  <?php
+    include('includes/imp/conn.php');
+    $pid = $_GET['pid']; // vendor provided respondan id
+    $gid = $_GET['gid']; // project id
     $vid = $_GET['vid']; // vendor id
-
-
-    // Function to get the client IP address
-if (!empty($_SERVER['HTTP_CLIENT_IP']))   
-  {
-    $ip_address = $_SERVER['HTTP_CLIENT_IP'];
+ 
+ // Variable check
+ if(true){
+  $stop = false;
+  if(!isset($_GET['gid']) || $_GET['gid'] == ''){
+   echo 'gid variable not set <br>';
+   $stop = true;
   }
-//whether ip is from proxy
-elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))  
-  {
-    $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+  if(!isset($_GET['vid']) || $_GET['vid'] == ''){
+   echo 'vid variable not set <br>';
+   $stop = true;
   }
-//whether ip is from remote address
-else
-  {
-    $ip_address = $_SERVER['REMOTE_ADDR'];
+  if(!isset($_GET['pid']) || $_GET['pid'] == ''){
+   echo 'pid variable not set <br>';
+   $stop = true;
   }
-    $ip = $ip_address;
-
-
-
-   //funtion to get date when link is punched
-    if(function_exists('date_default_timezone_set')) {
-    date_default_timezone_set("Asia/Kolkata");
-	}
-	$date = date("d/m/Y");
-	$date1 =  date("H:i a");
-	
-
-    // $conn = new mysqli("localhost","Username","Password","DBname");
-    if ($conn->connect_error) 
-    {
-    
-    die("Connection failed: " . $conn->connect_error);
-		
-	}
-
-	$sql = "INSERT INTO surveys(project_id,vendor_id,status,ip_address,vendor_resp_id,client_resp_id) VALUES ('$gid','$vid','start','$ip','$pid','$client_resp_id')";
-    if(isset($_GET['gid']) && isset($_GET['vid']) ){
-        if ($conn->query($sql) === TRUE) {
-            // echo "data entered";
-            $sql2 = "SELECT survey_link FROM surveys INNER JOIN projects ON surveys.project_id = projects.id WHERE surveys.project_id = '$gid' LIMIT 1";
-            $query_run = mysqli_query($conn, $sql2);
-            // echo 'asdf';
-            foreach($query_run as $row2)
-                { $survey = $row2['survey_link'] .$client_resp_id;
-                    ?>
-                <?php echo $survey; ?>
-
-                <?php } 
-            
-            echo "<script>window.location.href='$survey';</script>";
-            exit;
-        
-            
+ }
+ // get ip address
+ if(true){
+  // Function to get the client IP address
+  if (!empty($_SERVER['HTTP_CLIENT_IP']))   
+  {
+   $ip_address = $_SERVER['HTTP_CLIENT_IP'];
+  }
+  //whether ip is from proxy
+  elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))  
+  {
+   $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+  }
+   //whether ip is from remote address
+  else
+  {
+   $ip_address = $_SERVER['REMOTE_ADDR'];
+  }
+  $ip = $ip_address;
+ }
+ // get data and time
+ if(true){
+  if(function_exists('date_default_timezone_set')) {
+   date_default_timezone_set("Asia/Kolkata");
+  }
+  $start_time = date("d-m-Y H:i:s a");
+  // echo $date;
+ }
+ // get status
+ if(true){
+  $status = '0';
+ }
+ // get survey link
+ if(true){
+  if(!$stop){
+   $sql = "SELECT survey_link FROM surveys INNER JOIN projects ON surveys.project_id = projects.id WHERE surveys.project_id = '$gid' LIMIT 1";
+   if(mysqli_query($conn, $sql)){
+    $result = mysqli_fetch_array(mysqli_query($conn, $sql));
+    $survey = $result['survey_link'];
+    echo $survey;
+   }else{
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+   }
+  }
+  // echo $survey . '<br>';
+ }
+ // set cookie
+ if(true){
+  setcookie("pid", $pid, time()+60*60*24*30, "/");
+  setcookie("vid", $vid, time()+60*60*24*30, "/");
+  setcookie("ip", $ip, time()+60*60*24*30, "/");
+  setcookie("gid", $gid, time()+60*60*24*30, "/");
+ }
+ // sql query
+ if(true){
+  if(!$stop){
+   $sql = "INSERT INTO surveys(project_id, vendor_id, status, start_time, ip_address, vendor_resp_id) VALUES ('$gid', '$vid', '$status', '$start_time', '$ip', '$pid')";
+   if(mysqli_query($conn,$sql)){
+    $last_id = mysqli_insert_id($conn);
+    $sql = "update surveys set client_resp_id='$last_id' where id='$last_id'";
+    if(mysqli_query($conn,$sql)){
+     // echo "data entered";
     }
-} 
-else {
+    $survey = str_replace('{{id}}', $last_id, $survey);
+    echo "<script>window.location.href='$survey';</script>";
+   }else{
     echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
+   }
+  }
+ }
 $conn->close();  
 
-
-// then use the date functions, not the other way around
-
 ?>
-
-
-
 </body>
 </html>
